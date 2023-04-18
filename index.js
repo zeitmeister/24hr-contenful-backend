59387996b861bb9ee8a4e06adf72d954e2385a5b
simon <simon.sporrong@24hr.se>
2023-04-18 11:53:18 +0200

    stuff

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 
 app.post('/contentful-webhook', (req, res) => {
   const payload = req.body;
-
+  console.log(req.body, ' this is the req.body');
+  console.log(req, ' this is the req');
   // Parse the payload and process the data
   console.log('Received webhook payload:', payload);
-
+  
   res.status(200).send('Webhook received successfully');
 });
 
