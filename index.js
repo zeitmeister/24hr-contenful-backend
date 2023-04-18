d025d1688b321fdb5795f2c3e0cc54360c53aa19
simon <simon.sporrong@24hr.se>
2023-04-18 11:01:29 +0200

    FIRST COMMIT

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.post('/contentful-webhook', (req, res) => {
+  const payload = req.body;
+
+  // Parse the payload and process the data
+  console.log('Received webhook payload:', payload);
+
+  res.status(200).send('Webhook received successfully');
+});
+
+app.listen(3001, () => {
+  console.log('Server listening on port 3000');
+});
+
